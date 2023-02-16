2a8255d74d5e58e42ed128f3e36e4d355efa8cb2
sasipim2002 <64309010042@tnk.ac.th>
2023-02-17 00:16:37 +0700

    Update ex1.js

--- Week5/ex1.js
+++ Week5/ex1.js
@@ -1,13 +1,14 @@
-let items ={
-    "A4paper":120,
-    "Ruler":25,
-    "FlashDrive":130
+let items = {
+    "A4paper": 120,
+    "Ruler": 25,
+    "FlashDrive": 130
 }
-//Mr. Lee
-console.log("Mr. Lee",items.A4paper*5 +items.Ruler*2)
 
-//Mr. Yue
-console.log("Mr. Yue",items.FlashDrive*2 +items.A4paper*3)
+//Mr.Lee
+console.log("Mr.Lee", items.A4paper * 5 + items.Ruler * 2)
 
-//Ms.Pond
-console.log("Ms.Pond ",items.FlashDrive*2 +items.A4paper*3+items.Ruler*2)
+//Mr.Yue
+console.log("Mr.Yue", items.FlashDrive * 2 + items.A4paper * 3)
+
+//Mr.Bigg
+console.log("Mr.Bigg", items.A4paper * 5 + items.Ruler * 2 + items.FlashDrive * 10)
